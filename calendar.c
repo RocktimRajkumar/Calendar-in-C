771b4e2e50cd6dbe9ea893da30b48103a97d51e1
Rocktim <rocktim53@gmail.com>
2018-05-25 11:30:25 +0530

    comment added

--- calendar.c
+++ calendar.c
@@ -47,11 +47,13 @@ int main()
         scanf("%d%d",&nyr,&nmonth);
         if(nyr<1945)
         {
+            //If year less than 1945
             printf("\n\t Please enter year above 1945\n");
             continue;
         }
         else
         {
+            //If year greater than equal to 1945
             calendar(nyr,nmonth);
         }
         while(1)
@@ -63,8 +65,10 @@ int main()
         switch(ch)
         {
         case 80: //-------- DOWN ARROW -----------
+               //Increasing month
                 if(nmonth==12)
                 {
+                    //Jump to next year if month is december
                      nmonth=1;
                      nyr++;
                 }
@@ -75,12 +79,15 @@ int main()
                 calendar(nyr,nmonth);
                 break;
         case 77: //-------- RIGHT ARROW ----------
+            //Increasing Year
                 nyr++;
                 calendar(nyr,nmonth);
                 break;
         case 72: //------- UP ARROW -------------
+            // Decreasing Month
                 if(nmonth==1)
                 {
+                    // Jump to previous year if month is january
                     nyr--;
                     nmonth=12;
                 }
@@ -89,8 +96,10 @@ int main()
                 calendar(nyr,nmonth);
                 break;
         case 75: //-------- LEFT ARROW ----------
+            //Decreasing year
                 if(nyr==1945)
                 {
+                    //If year is 1945 and we click left arrow than
                   gotoxy(15,2);printf("Year below 1945 not available");
                 }
                 else
@@ -101,10 +110,12 @@ int main()
 
                 break;
         case 27: //--------- ESC KEY ------------
+            //Exit program
                 system("cls");
                 gotoxy(50,14);printf("Exiting program.\n\n\n\n\n");
                 exit(0);
         case 112://---------- p KEY ------------
+            //Go to particular year and month
                 system("cls");
                 goto enteryear;
         }
@@ -121,70 +132,75 @@ int main()
 void display(int nyr,int nmonth,int tdays,int days[])
 {
     int i,j,pos;
-    SetColor(12);
-    gotoxy(56,6);printf("%s %d",month[nmonth-1],nyr);
+    SetColor(12); //Color red
+    gotoxy(56,6);printf("%s %d",month[nmonth-1],nyr); //Heading year and month dispalying
     for(i=0,pos=30;i<7;i++,pos+=10)
     {
          if(i==6)
-            SetColor(9);
+            SetColor(9); //Sunday color blue
          else
-            SetColor(10);
+            SetColor(10);  //Others day color green
         gotoxy(pos,8);printf("%s",week[i]);
     }
 
-    SetColor(15);
+    SetColor(15); //setting the color white
 
-   tdays++;
+   tdays++; //incrementing the tdays by 1
     if(tdays==0 || tdays==7)
-        pos=91;
+        pos=91; //if tdays is 0 or 7, position is sunday
     if(tdays==1)
-        pos=31;
+        pos=31; //if tdays is 1, position is monday
     if(tdays==2)
-        pos=41;
+        pos=41;  //if tdays is 2, position is tuesday
     if(tdays==3)
-        pos=51;
+        pos=51;  //if tdays is 3, position is wednesday
     if(tdays==4)
-        pos=61;
+        pos=61;  //if tdays is 4, position is thursday
     if(tdays==5)
-        pos=71;
+        pos=71;  //if tdays is 5, position is friday
     if(tdays==6)
-        pos=81;
+        pos=81;  //if tdays is 6, position is saturday
 
     for(i=0,j=10,pos;i<days[nmonth-1];i++,pos+=10)
     {
         if(pos==91)
-            SetColor(8);
+            SetColor(8); //Changing color to dark grey for sunday
         else
-            SetColor(7);
+            SetColor(7); //Changing color to white for all days
 
         gotoxy(pos,j);printf("%d",i+1);
         if(pos==91)
         {
-            pos=21;
-            j++;
+            pos=21; //Moving position to monday
+            j++;  //Increasing j by 10 if position is sunday
         }
     }
 
-    SetColor(5);
+    SetColor(5); //Changing color to purple
 
+    //Drawing horizontal line
     for(i=22;i<102;i++)
     {
         gotoxy(i,4);printf("%c",196);
         gotoxy(i,17);printf("%c",196);
     }
 
+    //Drawing all the corner of the rectangle
     gotoxy(21,4);printf("%c",218);
     gotoxy(102,4);printf("%c",191);
     gotoxy(21,17);printf("%c",192);
     gotoxy(102,17);printf("%c",217);
 
+    //Drawing vertical line
     for(i=5;i<17;i++)
     {
         gotoxy(21,i);printf("%c",179);
         gotoxy(102,i);printf("%c",179);
     }
 
-    SetColor(11);
+    SetColor(11); //Changing color to aqua
+
+    //Drawing left and the right navigation symbol
     gotoxy(24,11);printf("%c",174);
     gotoxy(98,11);printf("%c",175);
 
@@ -211,29 +227,29 @@ void calendar(int nyr,int nmonth)
     int days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     int tdays=0,k,myear;
     system("cls");
-    myear=nyr-1;
+    myear=nyr-1; //Decreasing year by 1
             if(myear>=1945)
             {
                 for(k=1945;k<=myear;k++)
                 {
-                    if(k%4==0)
-                        tdays=tdays+366;
-                    else
-                        tdays=tdays+365;
+                    if(k%4==0) //If the year is a leap year than total no of days is 366
+                        tdays=tdays+366;  //counting all the days till nyr - 1
+                    else //If the year is a leap year than total no of days is 365
+                        tdays=tdays+365;  //counting all the days till nyr - 1
                 }
             }
 
             if(nyr%4==0)
-                days[1]=29;
+                days[1]=29; //changing value in days array from 28 to 29 since leap year
             else
-                days[1]=28;
+                days[1]=28; //changing value in days array from 29 to 28 since not a leap year
 
             for(k=0;k<(nmonth-1);k++)
             {
 
-                tdays=tdays+days[k];
+                tdays=tdays+days[k]; //Adding nmonth-1 days to tdays
             }
 
-            tdays=tdays%7;
-            display(nyr,nmonth,tdays,days);
+            tdays=tdays%7; //Finding the remainder of tdays so it can calculate the position to display
+            display(nyr,nmonth,tdays,days); //Passing the value to display
 }
